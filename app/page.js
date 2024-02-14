fe8a856483514f419258aac1fce7550b6f39113a
yogesh- <yogesh.gupta38@gmail.com>
2024-02-14 18:58:13 +0530

    added key to page.js

--- app/page.js
+++ app/page.js
@@ -6,24 +6,6 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [properties, setProperties] = useState([]);
-  const property_data = [
-    {
-      rent: "83,000",
-      location: "Bandra Kurla Complex,Mumbai",
-      verified: true,
-      seats: "25-40 seats",
-      carpet_area: "2339",
-      image: "/bkc.png",
-    },
-    {
-      rent: "183,000",
-      location: "Nariman Point,Mumbai",
-      verified: true,
-      seats: "80-90 seats",
-      carpet_area: "5445",
-      image: "/bkc.png",
-    },
-  ];
 
   const [filter, setFilter] = useState(false);
 
@@ -31,9 +13,7 @@ export default function Home() {
     async function fetchData() {
       const response = await fetch("api/");
       const data = await response.json();
-      console.log(data, "before state ");
       setProperties(data.data);
-      console.log(data, "from useEffect");
     }
 
     fetchData();
@@ -47,7 +27,7 @@ export default function Home() {
       {properties.length > 0 &&
         properties.map((property, index) => {
           return (
-            <div className="m-auto gap-2">
+            <div key={index} className="m-auto gap-2">
               <PropertyCard property={property} index={index} />
             </div>
           );
