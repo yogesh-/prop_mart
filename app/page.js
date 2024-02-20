e84fa077fbf0561d6d88c093a6d68e3981de0ac9
yogesh- <yogesh.gupta38@gmail.com>
2024-02-20 19:54:27 +0530

    changed url

--- app/page.js
+++ app/page.js
@@ -12,11 +12,15 @@ export default function Home() {
   useEffect(() => {
     async function fetchData() {
       if (filter) {
-        const response = await fetch("api/?verified=true");
+        const response = await fetch(
+          "https://qojvk62gs8.execute-api.ap-south-1.amazonaws.com/test/my-fetch-resource/?verified=true"
+        );
         const data = await response.json();
         setProperties(data.data);
       } else {
-        const response = await fetch("api/");
+        const response = await fetch(
+          "https://qojvk62gs8.execute-api.ap-south-1.amazonaws.com/test/my-fetch-resource"
+        );
         const data = await response.json();
         setProperties(data.data);
       }
